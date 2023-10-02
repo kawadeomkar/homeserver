81302701ce3238525757e2c1d5ab4fc3fce73cf7
Omkar Kawade <kawade.omkar21@gmail.com>
2023-10-01 23:56:29 -0700

    update minikube dev script

--- k8srun.sh
+++ k8srun.sh
@@ -2,6 +2,13 @@
 
 minikube start --mount-string="/Users/Omkar/opt/hass:/opt/hass" --mount
 minikube addons enable ingress
+minikube addons enable ingress-dns
+minikube addons enable storage
+minikube addons enable registry
+minikube addons enable dashboard
+minikube addons enable metrics-server
 
 kubectl apply -f k8s/ingress
 kubectl apply -f k8s/homeassistant
+
+minikube dashboard
