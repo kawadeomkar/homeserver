7a5b9055feb786b5fa4de5d1028d51e89e5fa886
Omkar Kawade <kawade.omkar21@gmail.com>
2023-10-02 04:38:33 -0700

    Update minikube run script, update microk8s channel

--- k8srun.sh
+++ k8srun.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-minikube start --mount-string="/Users/Omkar/opt/hass:/opt/hass" --mount
+minikube start --kubernetes-version v1.26.3 --mount-string="/Users/Omkar/opt/hass:/opt/hass" --mount
 minikube addons enable ingress
 minikube addons enable ingress-dns
 minikube addons enable registry
 minikube addons enable dashboard
 minikube addons enable metrics-server
 
-kubectl apply -f k8s/ingress
-kubectl apply -f k8s/homeassistant
-
-minikube dashboard
+minikube kubectl -- apply -f k8s/ingress
+minikube kubectl -- apply -f k8s/homeassistant
