534f6e91a02297bc4e7a50e1f282131cc50f28ba
Omkar Kawade <kawade.omkar21@gmail.com>
2023-10-02 02:42:39 -0700

    k8s WIP - test on RPI

--- k8srun.sh
+++ k8srun.sh
@@ -3,7 +3,6 @@
 minikube start --mount-string="/Users/Omkar/opt/hass:/opt/hass" --mount
 minikube addons enable ingress
 minikube addons enable ingress-dns
-minikube addons enable storage
 minikube addons enable registry
 minikube addons enable dashboard
 minikube addons enable metrics-server
